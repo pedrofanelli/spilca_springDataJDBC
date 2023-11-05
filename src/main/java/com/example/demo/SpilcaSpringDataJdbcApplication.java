3e286c8857199c70686a580b4d9a16a2311ce6ad
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-05 16:55:29 -0300

    working!!

--- src/main/java/com/example/demo/SpilcaSpringDataJdbcApplication.java
+++ src/main/java/com/example/demo/SpilcaSpringDataJdbcApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"controllers","repositories","services"})
+@ComponentScan(basePackages={"controllers","services"})
+@EnableJdbcRepositories(basePackages={"repositories"})
 public class SpilcaSpringDataJdbcApplication {
 
 	public static void main(String[] args) {
