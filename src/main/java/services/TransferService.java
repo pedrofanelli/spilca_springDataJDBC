ecfd17359c6fd72e049bd0d0bb5e8415540b0ee9
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-05 16:28:44 -0300

    servicio armada

--- src/main/java/services/TransferService.java
+++ src/main/java/services/TransferService.java
@@ -15,10 +15,37 @@ public class TransferService {
 
 	private final AccountRepository repo;
 	
+	// aunque sea una interfaz, Spring Data la implementará y se agregará al contexto
 	public TransferService(AccountRepository repo) {
 		this.repo = repo;
 	}
 	
+	/*
+	 * Tanto .findById() como .findAll() se implementan solos por Spring Data
+	 */
+	@Transactional(rollbackFor= {SQLException.class,Exception.class})
+	public void transferMoney(long idSender, long idReceiver, BigDecimal amount) throws SQLException {
+
+		Account sender = repo.findById(idSender).orElseThrow(()->new RuntimeException());
+		Account receiver = repo.findById(idReceiver).orElseThrow(()->new RuntimeException());
+		
+		BigDecimal senderNewAmount = sender.getAmount().subtract(amount);
+		BigDecimal receiverNewAmount = receiver.getAmount().add(amount);
+		
+		repo.changeAmount(idSender, senderNewAmount);
+		repo.changeAmount(idReceiver, receiverNewAmount);
+		
+		//throw new RuntimeException("OH NO! UNA RUNTIME EXCEPTION! ROLLBACK!!!");
+		
+	}
 	
+	public Iterable<Account> getAllAccounts() {
+		return repo.findAll();
+	}
+	
+	//usamos la búsqueda custom que armamos; otra custom es .changeAmount()
+	public List<Account> findAccountsByName(String name) {
+		return repo.findAccountsByName(name);
+	}
 	
 }
