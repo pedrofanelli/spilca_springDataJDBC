11fdfafcb3ea5aafa8e383f8be0a40ec718e4604
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-06 18:50:26 -0300

    building ut

--- src/test/java/services/TransferServiceUnitTests.java
+++ src/test/java/services/TransferServiceUnitTests.java
@@ -1,5 +1,27 @@
 package services;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import repositories.AccountRepository;
+
+
+/*
+ * We eliminate all the dependencies and isolate one piece of logic
+ * Dependencies are the methods parameters and object instances with an external origin
+ * The external objects are represented with "mocks"
+ */
 public class TransferServiceUnitTests {
 
+	@Test
+	public void moneyTransferHappyFlow() {
+		// we create a mock to manipulate
+		AccountRepository accountRepository = mock(AccountRepository.class);           
+	 
+		// we create a new instance of the object which contains the method we want to test
+		// passing our mock!
+	    TransferService transferService = new TransferService(accountRepository);
+	}
+	
 }
