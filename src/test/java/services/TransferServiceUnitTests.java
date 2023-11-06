ca4f8f8211dcb0698fa24259cd54c63f74ae9c04
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-06 18:53:17 -0300

    names

--- src/test/java/services/TransferServiceUnitTests.java
+++ src/test/java/services/TransferServiceUnitTests.java
@@ -2,6 +2,7 @@ package services;
 
 import static org.mockito.Mockito.mock;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import repositories.AccountRepository;
@@ -14,7 +15,11 @@ import repositories.AccountRepository;
  */
 public class TransferServiceUnitTests {
 
+	/*
+	 * Each test represents a single unit of logic being tested, in this case the happy flow!
+	 */
 	@Test
+	@DisplayName("Test the amount is transferred from one account to another if no exception occurs.")
 	public void moneyTransferHappyFlow() {
 		// we create a mock to manipulate
 		AccountRepository accountRepository = mock(AccountRepository.class);           
