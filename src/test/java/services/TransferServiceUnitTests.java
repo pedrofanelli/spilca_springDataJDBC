2161d10bbb92f81a9162317c902987d9eba698af
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-06 19:34:36 -0300

    done

--- src/test/java/services/TransferServiceUnitTests.java
+++ src/test/java/services/TransferServiceUnitTests.java
@@ -10,6 +10,11 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import models.Account;
 import repositories.AccountRepository;
 
@@ -18,8 +23,17 @@ import repositories.AccountRepository;
  * Dependencies are the methods parameters and object instances with an external origin
  * The external objects are represented with "mocks"
  */
+@ExtendWith(MockitoExtension.class)
 public class TransferServiceUnitTests {
 
+	// create a mock object
+	@Mock
+	private AccountRepository accountRepository;
+	
+	// create the object and inject the mock objects annotated
+	@InjectMocks
+	private TransferService transferService;
+	
 	/*
 	 * Each test represents a single unit of logic being tested, in this case the happy flow!
 	 */
@@ -27,11 +41,14 @@ public class TransferServiceUnitTests {
 	@DisplayName("Test the amount is transferred from one account to another if no exception occurs.")
 	public void moneyTransferHappyFlow() throws SQLException {
 		// we create a mock to manipulate
-		AccountRepository accountRepository = mock(AccountRepository.class);           
-	 
+		//AccountRepository accountRepository = mock(AccountRepository.class);           
+		// we can also use annotations!
+		
+		
 		// we create a new instance of the object which contains the method we want to test
 		// passing our mock!
-	    TransferService transferService = new TransferService(accountRepository);
+	    //TransferService transferService = new TransferService(accountRepository);
+		//also with annotations!
 	    
 	    //we use the .given() method to tell how to behave a method from a mock object
 	    // remember in the service we used the repository to find 2 accounts
@@ -45,8 +62,21 @@ public class TransferServiceUnitTests {
 	    given(accountRepository.findById(sender.getId())).willReturn(Optional.of(sender));
 	    given(accountRepository.findById(destination.getId())).willReturn(Optional.of(destination));
 
+	    
+	    /*
+	     * UP to here it's the ASSUMPTIONS FASE: we control the dependencies
+	     */
+	    
+	    /*
+	     * Then the CALL: we call the method object of test
+	     */
+	    
 	    // we call the method we want with all the data
 	    transferService.transferMoney(sender.getId(), destination.getId(), new BigDecimal(100));
+
+	    /*
+	     * VALIDATIONS: we validate that the executed method had the expected behavior
+	     */
 	    
 	    // we verify that these methods were called with the corresponding arguments
 	    verify(accountRepository).changeAmount(3, new BigDecimal(900));
